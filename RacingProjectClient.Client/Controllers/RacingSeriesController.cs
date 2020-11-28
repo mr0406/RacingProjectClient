3d5a07648507ccc3f48dc236e258fd16729b77da
mr0406 <64924553+mr0406@users.noreply.github.com>
2020-11-28 14:06:35 +0100

    Add RacingSerie model, first succesful consume api

---
+++ RacingProjectClient.Client/Controllers/RacingSeriesController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RacingProjectClient.Client.ApiModels;
+using RacingProjectClient.Client.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace RacingProjectClient.Client.Controllers
+{
+    [Route("[controller]")]
+    public class RacingSeriesController : Controller
+    {
+        private RacingApi _racingApi = new RacingApi();
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            List<RacingSerie> racingSeries = new List<RacingSerie>();
+
+            HttpClient client = _racingApi.Initial();
+            HttpResponseMessage response = await client.GetAsync("/racingseries");
+
+            if(response.IsSuccessStatusCode)
+            {
+                var result = response.Content.ReadAsStringAsync().Result;
+                racingSeries = JsonConvert.DeserializeObject<List<RacingSerie>>(result);
+            }
+
+            return View(racingSeries);
+        }
+    }
+}
---
+++ RacingProjectClient.Client/Helper/RacingApi.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace RacingProjectClient.Client.Helper
+{
+    public class RacingApi
+    {
+        public HttpClient Initial()
+        {
+            var Client = new HttpClient();
+            Client.BaseAddress = new Uri("http://localhost:52746");
+            return Client;
+        }
+    }
+}
