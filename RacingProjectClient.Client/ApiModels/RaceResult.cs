128de49566ea229e2fa979d72e0f346baeb6a115
mr0406 <64924553+mr0406@users.noreply.github.com>
2020-11-28 20:40:53 +0100

    Add ApiModels

---
+++ RacingProjectClient.Client/ApiModels/RaceResult.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RacingProjectClient.Client.ApiModels
+{
+    public class RaceResult
+    {
+        public int Id { get; set; }
+
+        [Required, ForeignKey("Driver")]
+        public int DriverId { get; set; }
+        [Required, ForeignKey("Race")]
+        public int RaceId { get; set; }
+
+        public int StartingPosition { get; set; }
+        public int FinalPosition { get; set; }
+        public int ScoredPoints { get; set; }
+
+        public Driver Driver { get; set; }
+        public Race Race { get; set; }
+    }
+}
