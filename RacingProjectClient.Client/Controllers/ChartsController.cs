4752cc87b8ce069d3e4c6e94b81d7f27f95a5dc1
mr0406 <64924553+mr0406@users.noreply.github.com>
2020-12-11 17:51:28 +0100

    2 charts done

--- RacingProjectClient.Client/Controllers/ChartsController.cs
+++ RacingProjectClient.Client/Controllers/ChartsController.cs
@@ -1,14 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using RacingProjectClient.Client.ApiModels;
+using RacingProjectClient.Client.Helper;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace RacingProjectClient.Client.Controllers
 {
     public class ChartsController : Controller
     {
+        private RacingApi _racingApi = new RacingApi();
+
         [HttpGet]
         public async Task<IActionResult> Index()
         {
@@ -18,13 +23,40 @@ namespace RacingProjectClient.Client.Controllers
         [HttpGet]
         public async Task<IActionResult> Teams()
         {
-            List<ChartElement> chartElements = new List<ChartElement>();
-            chartElements.Add(new ChartElement("Ferrari", 2));
-            chartElements.Add(new ChartElement("Mercedes", 3));
-            chartElements.Add(new ChartElement("RedBull", 2));
+            HttpClient client = _racingApi.Initial();
+            HttpResponseMessage response = await client.GetAsync($"/charts/teams");
 
-            ViewData["Chart_Title"] = "Number of drivers in F1 teams";
-            return View("~/Views/Charts/Chart.cshtml", chartElements);
+            if (response.IsSuccessStatusCode)
+            {
+                var result = response.Content.ReadAsStringAsync().Result;
+                List<ChartElement> chartElements = JsonConvert.DeserializeObject<List<ChartElement>>(result);
+
+                ViewData["Chart_Title"] = "Number of drivers in F1 teams";
+
+                return View("~/Views/Charts/Chart.cshtml", chartElements);
+            }
+
+            return NotFound();
         }
+
+        [HttpGet]
+        public async Task<IActionResult> RacingSeries()
+        {
+            HttpClient client = _racingApi.Initial();
+            HttpResponseMessage response = await client.GetAsync($"/charts/racingSeries");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var result = response.Content.ReadAsStringAsync().Result;
+                List<ChartElement> chartElements = JsonConvert.DeserializeObject<List<ChartElement>>(result);
+
+                ViewData["Chart_Title"] = "Number of teams in racing series";
+
+                return View("~/Views/Charts/Chart.cshtml", chartElements);
+            }
+
+            return NotFound();
+        }
+
     }
 }
