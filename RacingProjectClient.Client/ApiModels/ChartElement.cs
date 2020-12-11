4bfba59f91106e7ce4cb39e02466035e4b3cc658
mr0406 <64924553+mr0406@users.noreply.github.com>
2020-12-11 16:32:31 +0100

    First chart

---
+++ RacingProjectClient.Client/ApiModels/ChartElement.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RacingProjectClient.Client.ApiModels
+{
+    public class ChartElement
+    {
+        public string Name { get; set; }
+        public int Amount { get; set; }
+
+        public ChartElement()
+        {
+
+        }
+
+        public ChartElement(string name, int amount)
+        {
+            Name = name;
+            Amount = amount;
+        }
+    }
+}
---
+++ RacingProjectClient.Client/Controllers/ChartsController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RacingProjectClient.Client.ApiModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RacingProjectClient.Client.Controllers
+{
+    public class ChartsController : Controller
+    {
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Teams()
+        {
+            List<ChartElement> chartElements = new List<ChartElement>();
+            chartElements.Add(new ChartElement("Ferrari", 2));
+            chartElements.Add(new ChartElement("Mercedes", 3));
+            chartElements.Add(new ChartElement("RedBull", 2));
+
+            ViewData["Chart_Title"] = "Number of drivers in F1 teams";
+            return View("~/Views/Charts/Chart.cshtml", chartElements);
+        }
+    }
+}
